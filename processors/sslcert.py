5978f2527ef766eeacc8906a215ac1c0de4e7cb5
Hacktix <zecmad16@htlkaindorf.at>
2023-12-15 19:58:10 +0100

    implement ssl cert processor

---
+++ processors/sslcert.py
@@ -0,0 +1,29 @@
+import warnings
+from cryptography.utils import CryptographyDeprecationWarning
+warnings.filterwarnings('ignore',  category=CryptographyDeprecationWarning)
+
+from nodes import NodeBase, Website
+from processor import ProcessorBase
+from cryptography import x509
+from cryptography.x509.oid import ExtensionOID
+import ssl
+
+class SSLCertProcessor(ProcessorBase):
+    consumed_nodetypes = [Website]
+
+    def process(self) -> list[NodeBase]:
+        nodes = []
+        certpem = ssl.get_server_certificate((self._get_domain(), 443))
+        cert = x509.load_pem_x509_certificate(certpem.encode("utf-8"))
+
+        alt_names = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME).value.get_values_for_type(x509.DNSName)
+        for name in alt_names:
+            if "*." in name:
+                continue
+            nodes.append(Website(name))
+
+        return nodes
+    
+    def _get_domain(self) -> str:
+        match(self.node.__class__.__name__):
+            case "Website": return self.node.domain
