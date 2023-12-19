0381444822545557fa339e9b1d89fb9590c49d6f
Hacktix <zecmad16@htlkaindorf.at>
2023-12-19 16:35:35 +0100

    extract helper map generation to util file

--- session.py
+++ session.py
@@ -1,13 +1,13 @@
-import sys, inspect
 from config import KeyscoreConfig
 from nodes import NodeBase
 from processor import ProcessorBase
 from processors import *
+from util.module_util import get_nodeclass_map, get_processor_consume_map
 
 class KeyscoreSession():
 
-    _nodeclass_map = {}
-    _processor_consume_map = {}
+    _nodeclass_map = get_nodeclass_map()
+    _processor_consume_map = get_processor_consume_map()
 
     def __init__(self, start_nodes: list[NodeBase]) -> None:
         self.start_nodes = start_nodes.copy()
@@ -61,32 +61,3 @@ class KeyscoreSession():
             if node.equals(queued):
                 return False
         return True
-
-
-
-modules = list(sys.modules.values())
-for module in modules:
-    try:
-        all_node_members = inspect.getmembers(module)
-        for ncl in all_node_members:
-            if not inspect.isclass(ncl[1]) or ncl[1] is NodeBase:
-                continue
-            if issubclass(ncl[1], NodeBase):
-                KeyscoreSession._nodeclass_map[ncl[0]] = ncl[1]
-    except:
-        pass
-
-all_processor_members = inspect.getmembers(sys.modules["processors"])
-for pm in all_processor_members:
-    if not inspect.ismodule(pm[1]):
-        continue
-    all_pmodule_members = inspect.getmembers(pm[1])
-    for pcl in all_pmodule_members:
-        if not inspect.isclass(pcl[1]) or pcl[1] is ProcessorBase:
-            continue
-        if issubclass(pcl[1], ProcessorBase):
-            for consumed in pcl[1].consumed_nodetypes:
-                if consumed in KeyscoreSession._processor_consume_map:
-                    KeyscoreSession._processor_consume_map[consumed].append(pcl[1])
-                else:
-                    KeyscoreSession._processor_consume_map[consumed] = [pcl[1]]
---
+++ util/module_util.py
@@ -0,0 +1,38 @@
+import sys
+import inspect
+
+from nodes import NodeBase
+from processor import ProcessorBase
+
+def get_nodeclass_map():
+    nodeclass_map = {}
+    modules = list(sys.modules.values())
+    for module in modules:
+        try:
+            all_node_members = inspect.getmembers(module)
+            for ncl in all_node_members:
+                if not inspect.isclass(ncl[1]) or ncl[1] is NodeBase:
+                    continue
+                if issubclass(ncl[1], NodeBase):
+                    nodeclass_map[ncl[0]] = ncl[1]
+        except:
+            pass
+    return nodeclass_map
+
+def get_processor_consume_map():
+    processor_consume_map = {}
+    all_processor_members = inspect.getmembers(sys.modules["processors"])
+    for pm in all_processor_members:
+        if not inspect.ismodule(pm[1]):
+            continue
+        all_pmodule_members = inspect.getmembers(pm[1])
+        for pcl in all_pmodule_members:
+            if not inspect.isclass(pcl[1]) or pcl[1] is ProcessorBase:
+                continue
+            if issubclass(pcl[1], ProcessorBase):
+                for consumed in pcl[1].consumed_nodetypes:
+                    if consumed in processor_consume_map:
+                        processor_consume_map[consumed].append(pcl[1])
+                    else:
+                        processor_consume_map[consumed] = [pcl[1]]
+    return processor_consume_map
