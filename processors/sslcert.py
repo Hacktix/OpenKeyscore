9fe1ce8e075101301bbeff46e68e6666f0ffc416
Hacktix <zecmad16@htlkaindorf.at>
2023-12-23 12:17:18 +0100

    make sslcert processor consume emails

--- processors/sslcert.py
+++ processors/sslcert.py
@@ -2,15 +2,15 @@ import warnings
 from cryptography.utils import CryptographyDeprecationWarning
 warnings.filterwarnings('ignore',  category=CryptographyDeprecationWarning)
 
-from nodes import NodeBase, Website
+from nodes import Email, NodeBase, Website
 from processor import ProcessorBase
 from cryptography import x509
-from cryptography.x509.oid import ExtensionOID
+from cryptography.x509.oid import ExtensionOID, NameOID
 from util.domain_util import is_excluded_domain
 import ssl
 
 class SSLCertProcessor(ProcessorBase):
-    consumed_nodetypes = [Website]
+    consumed_nodetypes = [Website, Email]
 
     def process(self) -> list[NodeBase]:
         nodes = []
@@ -21,7 +21,8 @@ class SSLCertProcessor(ProcessorBase):
         try:
             certpem = ssl.get_server_certificate((domain, 443), timeout=3)
             cert = x509.load_pem_x509_certificate(certpem.encode("utf-8"))
-
+            subject = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)[0].value
+            nodes.append(Website(subject))
             alt_names = cert.extensions.get_extension_for_oid(ExtensionOID.SUBJECT_ALTERNATIVE_NAME).value.get_values_for_type(x509.DNSName)
             for name in alt_names:
                 if "*." in name:
@@ -35,3 +36,4 @@ class SSLCertProcessor(ProcessorBase):
     def _get_domain(self) -> str:
         match(self.node.__class__.__name__):
             case "Website": return self.node.domain
+            case "Email": return self.node.email.split("@")[1]
