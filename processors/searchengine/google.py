62cd1a2ed7f368d38e5e4ef6532896fb2a6dd2f3
Hacktix <zecmad16@htlkaindorf.at>
2024-01-11 18:06:42 +0100

    add error handling for google processor

--- processors/searchengine/google.py
+++ processors/searchengine/google.py
@@ -1,3 +1,4 @@
+from loguru import logger
 from nodes import NodeBase, Username
 from processor import ProcessorBase
 from googlesearch import search
@@ -8,13 +9,18 @@ class GoogleProcessor(ProcessorBase):
 
     def process(self) -> list[NodeBase]:
         username = self._get_queryable_username()
-        results = search(f'"{username}"', sleep_interval=1)
-        nodes = []
-        for result in results:
-            processor = get_search_result_processor(result)
-            if processor:
-                nodes = nodes + processor.get_nodes_from_search_result(result)
-        return nodes
+        try:
+            results = search(f'"{username}"', sleep_interval=1)
+            nodes = []
+            for result in results:
+                processor = get_search_result_processor(result)
+                logger.debug(f"Processing search result {result.url} with {processor.__class__.__name__}")
+                if processor:
+                    nodes = nodes + processor.get_nodes_from_search_result(result)
+            return nodes
+        except Exception as e:
+            logger.error(f"Google Lookup failed: {e}")
+            return []
 
     def _get_queryable_username(self) -> str:
         match(self.node.__class__.__name__):
